ef7dbe4c6b824b4ed8f23e838cc0449a8eeee740
Roberto Fernandez Toro <robertobouses@gmail.com>
2025-03-12 19:45:52 +0100

    feature: add match package with Match, Strategy, and Result structs

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/robertobouses/online-football-tycoon/match"
+)
 
 func main() {
 	fmt.Println("Online Football Tycoon")
+
+	m := match.Match{
+		HomeMatchStrategy: match.Strategy{
+			GameTempo: "fast",
+		},
+		AwayMatchStrategy: match.Strategy{
+			GameTempo: "slow",
+		},
+	}
+
+	result, _ := m.Play()
+	fmt.Println(result)
+
 }
---
+++ team/team.go
@@ -0,0 +1,7 @@
+package team
+
+type Team struct {
+	Name    string
+	Country string
+	Players []Player
+}
